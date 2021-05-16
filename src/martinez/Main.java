5f5982e39e6263f6c369e807b8cf8540f640dccd
Jhon Duarte <johnduarte7@gmail.com>
2021-05-15 20:07:39 -0500

    add my name

--- src/martinez/Main.java
+++ src/martinez/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Daniel");
         System.out.println("Hello Daniel, my name is Samuel.");
+        System.out.println("Hello Daniel, my name is Jhon.");
     }
 }
